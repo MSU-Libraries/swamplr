c1c0d6bb3a1ca86d1dbbe96da78cb8cfe44f146a
Sruthin Gaddam <gaddamsr@msu.edu>
2017-09-25 13:32:23 -0400

    added choices for frequency

--- swamplr_services/views.py
+++ swamplr_services/views.py
@@ -82,6 +82,7 @@ def add_service(request):
         new_service.description = form_data.cleaned_data['description'] 
         new_service.command = form_data.cleaned_data['command']
         if form_data.cleaned_data['frequency']:
+            print(form_data.cleaned_data['frequency'])
             frequency = form_data.cleaned_data['frequency']
             frequency_time  = form_data.cleaned_data['frequency_time']
             frequency = (frequency if frequency_time =='MIN' else frequency*60 if frequency_time =='HOUR' else  frequency*60*24 if frequency_time =='DAY' else  frequency*60*24*7 if frequency_time =='WEEK'  else 0)
