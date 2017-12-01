8a8fa3f035feff5a919174f416fec6595ad9cfa2
Devin Higgins <higgi135.mail.lib.msu.edu>
2017-12-01 15:54:17 -0500

    Working on namespaces

--- swamplr_namespaces/models.py
+++ swamplr_namespaces/models.py
@@ -25,3 +25,16 @@ class cache_job(models.Model):
     process_id = models.IntegerField(null=True)
     last_run = models.DateTimeField(null=True)
     
+class job_objects(models.Model):
+    """Table to record progress pid by pid."""
+    object_id = models.AutoField(primary_key=True)
+    job_id = models.ForeignKey('swamplr_jobs.jobs')
+    completed = models.DateTimeField()
+    # "Success" or "Failure" (or "Skip")
+    result_id = models.ForeignKey('object_results')
+    pid = models.CharField(max_length=64, null=True)
+
+class object_results(models.Model):
+
+    result_id = models.AutoField(primary_key=True)
+    label = models.CharField(max_length=32)
