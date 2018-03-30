6e2f39c1e72811c4f93fc2e18df204427ae45c98
Megan Schanz <schanzme@mail.lib.msu.edu>
2018-03-30 10:10:32 -0400

    Redesigned add service form to be multilined

--- swamplr_services/forms.py
+++ swamplr_services/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from swamplr_services.models import services
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Field
-
+from crispy_forms.layout import Submit, Layout, Field, Row
 
 class ServicesForm(ModelForm):
 
@@ -24,35 +23,40 @@ class ServicesForm(ModelForm):
         self.helper.help_text_inline = True
         self.helper.form_action = 'add-service'
         self.helper.layout = Layout(
-            "label",
-            "description",
-            "command",
-            "frequency",
-            Field("frequency_time",css_class=" btn btn-info dropdown-toggle" ),
-            "auto_archive",
-            Field("auto_archive_time",css_class=" btn btn-info dropdown-toggle" ),
+            Row("label"),
+            Row("description"),
+            Row("command"),
+            Row("frequency",
+                Field("frequency_time",css_class=" btn btn-info dropdown-toggle" ),
+                "auto_archive",
+                Field("auto_archive_time",css_class=" btn btn-info dropdown-toggle" )),
             "last_started",
-            "run_as_user",
-            Submit("add-service", "Add Service", css_class="btn btn-outline-success"),
+            Row("run_as_user"),
+            Row(Submit("add-service", "Add Service", css_class="btn btn-outline-success")),
         )
 
     run_as_user = forms.CharField(required=False, widget=forms.TextInput(
                                                       attrs={"placeholder": "User name",
                                                              "size": "15"}))
+
     label = forms.CharField(widget=forms.TextInput(
-                                                      attrs={"placeholder": "Label"}))
-    description = forms.CharField(widget=forms.TextInput(
-                                                      attrs={"placeholder": "Description"}))
-    command = forms.CharField(widget=forms.TextInput(
+                                                      attrs={"placeholder": "Label",
+                                                             "size": "30"}))
+    description = forms.CharField(widget=forms.Textarea(
+                                                      attrs={"placeholder": "Description",
+                                                             "cols": "60",
+                                                             "rows": "2"}))
+    command = forms.CharField(widget=forms.Textarea(
                                                       attrs={"placeholder": "Command",
-                                                             "size": "41"}))
+                                                             "cols": "60",
+                                                             "rows": "5"}))
     frequency = forms.CharField(required=False, widget=forms.TextInput(
                                                       attrs={"placeholder": "Frequency",
                                                              "size": "10"}))
     frequency_time = forms.ChoiceField(required=False,  choices=FREQUENCY_CHOICES )
     
     auto_archive = forms.CharField(required=False, widget=forms.TextInput(
-                                                      attrs={"placeholder": "Auto archive if success after...",
+                                                      attrs={"placeholder": "Auto archive",
                                                              "size": "10"}))
     auto_archive_time = forms.ChoiceField(required=False,  choices=FREQUENCY_CHOICES )
 
@@ -61,5 +65,3 @@ class ServicesForm(ModelForm):
     class Meta:
         model = services
         fields = ["label", "description", "command", "frequency", "auto_archive", "last_started", "run_as_user"]
-
-    
